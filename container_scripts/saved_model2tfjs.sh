439a2312d14e5139d0269d89000c43c6954d3d1b
Bernardo <bernardogcgalvao@gmail.com>
2020-01-27 16:56:48 +0000

    update conversion script to match new directory structure convention

--- container_scripts/saved_model2tfjs.sh
+++ container_scripts/saved_model2tfjs.sh
@@ -5,29 +5,24 @@ MODEL_NAME="sample_model"
 
 # DEFAULTS
 MODEL_DIR=./output/$MODEL_NAME
-SAVED_MODEL_DIR=$MODEL_DIR/saved_model
-OUTPUT_DIR=$MODEL_DIR/tensorflow_js
+SAVED_MODEL_DIR=$MODEL_DIR/export/frozen_graph/saved_model
+OUTPUT_DIR=$MODEL_DIR/export/tensorflow_js
 
 # safety checks
 if [ ! -d $MODEL_DIR ]
 then
   echo -e "The model and its associated directory \n\n\
   $MODEL_DIR\n\n do not exist."
+  exit -3
 fi 
 
-if [ ! -f $MODEL_DIR/saved_model.pb ]
-then
-  if [ ! -f $SAVED_MODEL_DIR/saved_model.pb ]
-    then
-      echo -e "saved_model.pb not available\n\n \
-      you can try an alternative method using tensorflowjs_wizard"
-      exit -3
-  fi
+if [ ! -f $SAVED_MODEL_DIR/saved_model.pb ]
+  then
+    echo -e "saved_model.pb not available\n\n \
+    you can try an alternative method using tensorflowjs_wizard"
+    exit -3
 fi
 
-echo -e "preparing to convert saved_model.pb...\n\n"
-mkdir -p $SAVED_MODEL_DIR/variables
-cp $MODEL_DIR/saved_model.pb $SAVED_MODEL_DIR/saved_model.pb
 
 echo -e "running tensorflowjs_converter with proposed default params of \
 \`tensorflowjs_wizard\` (try it!)...\n\n"
